cff747435aee765e96bbc966cf433492aa14a5b6
Mystic#9697 <shadow6060@users.noreply.github.com>
2023-03-12 21:36:32 -0600

    V14 mystics patch2 (#14)
    * Updated to v14 woo!
    
    Finally got v14 working and everything seems to work so far :D
    
    * Update schema.prisma
    
    * Add files via upload
    
    Add the Right files
    
    * Add files via upload
    
    * Add files via upload
    
    Made so we can either use a Url or attachment command
    
    * Add files via upload
    
    Fixed the statuses now we can have them :D
    
    * Add files via upload
    
    Supports having Two Number Lengths
    
    * Add files via upload
    
    * Add files via upload

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -10,11 +10,17 @@ import { arraysSimilar } from "../utils/array";
 import { IllegalStateError } from "../utils/error";
 import { OrderStatus } from "@prisma/client";
 
-export const snowflake = z.string().length(19).regex(/^\d+$/);
+export const snowflake = z.string().refine((value) => {
+	const isValidLength = value.length === 18 || value.length === 19;
+	const isValidRegex = /^\d+$/.test(value);
+	return isValidLength && isValidRegex;
+}, "Invalid snowflake value");
+
 const pFormattable = <T extends number = 1>(n: T = 1 as T) =>
 	z.string().refine(x => x.split("{}").length - 1 === n, {
 		message: `Formattable must contain ${n} placeholders`,
 	}) as z.ZodType<PositionalFormattable<T>>;
+
 const nFormattable = <T extends string[]>(...keys: T) =>
 	z.string().refine(
 		x =>
@@ -26,6 +32,12 @@ const nFormattable = <T extends string[]>(...keys: T) =>
 			message: `Formattable must contain the placeholders ${keys.join(", ")}`,
 		}
 	) as z.ZodType<NamedFormattable<T>>;
+
+export { snowflakee, pFormattable, nFormattable };
+
+
+
+
 
 const textSchema = z
 	.object({
